ba19078cd4ddc87e3c37a729d232c2d93ed0011d
Clément Garbay <clementgarbay@gmail.com>
2018-01-17 13:54:01 +0100

    Remove useless function

--- src/utils/str-utils.js
+++ src/utils/str-utils.js
@@ -1,5 +1,4 @@
 module.exports = {
-  toNumber: (str = '') => Number.parseFloat(str),
   extractNumber: (str = '') => {
     const number = str.match(/\d+/);
     return number ? number[0].trim() : '';
